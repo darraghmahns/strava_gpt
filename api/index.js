d610590208d9c91e5abdd12f6e0f5f0084a2028f
Darragh Mahns <darraghmahns@gmail.com>
2025-06-02 11:33:22 -0600

    updated .env again

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ const app = express();
 let access_token = '';
 
 app.get('/auth', (req, res) => {
-  const redirect = `https://www.strava.com/oauth/authorize?client_id=${process.env.STRAVA_CLIENT_ID}&response_type=code&redirect_uri=${process.env.REDIRECT_URI}&approval_prompt=force&scope=activity:read_all`;
+  const redirect = `https://www.strava.com/oauth/authorize?client_id=${process.env.STRAVA_CLIENT_ID}&response_type=code&redirect_uri=${process.env.STRAVA_REDIRECT_URI}&approval_prompt=force&scope=activity:read_all`;
   res.redirect(redirect);
 });
 
